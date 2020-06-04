e045d84e162a970e89a7943a334b2ce49e6faed7
Zhaoyi Ma <>
2020-06-04 19:03:49 -0400

    mysql config

--- findy/src/main/java/db/MySQLDBUtil.java
+++ findy/src/main/java/db/MySQLDBUtil.java
@@ -5,7 +5,7 @@ public class MySQLDBUtil {
 	private static final String PORT_NUM = "3306";
 	public static final String DB_NAME = "findy";
 	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "Gxjdsb2020!";
+	private static final String PASSWORD = "passwordooo!";
 	public static final String URL = "jdbc:mysql://"
 			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
